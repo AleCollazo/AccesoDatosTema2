218e84724189143c05495f0b62e1920af7b8dfb0
Alejandro Collazo <alecollazofdez@gmail.com>
2018-10-16 17:31:22 +0200

    Ejercicio2: Crear método mostrarTitulos

--- src/ejeciciosDom/Ejercicios.java
+++ src/ejeciciosDom/Ejercicios.java
@@ -4,6 +4,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class Ejercicios {
 	public Document crearArbol(String ruta) {
@@ -19,13 +20,31 @@ public class Ejercicios {
 		return doc;
 	}
 	
+	public void mostrarTitulos(NodeList pelis) {
+		NodeList hijos;
+		
+		for(int i = 0; i < pelis.getLength(); i++) {
+			hijos = pelis.item(i).getChildNodes();
+			for(int j = 0; j < hijos.getLength(); j++) {
+				if(hijos.item(j).getNodeName().equals("titulo")) {
+					System.out.println(hijos.item(j).getNodeName());
+					System.out.println(hijos.item(j).getFirstChild().getNodeValue());
+					System.out.println();
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		Ejercicios ejer = new Ejercicios();
 		
-		String ruta = System.getProperty("User.home")+"\\Documents\\peliculas.xml";
+		String ruta = System.getProperty("user.home")+"\\Documents\\peliculas.xml";
 		
 		Document doc = ejer.crearArbol(ruta);
+		
+		NodeList pelis = doc.getElementsByTagName("pelicula");
+		
+		ejer.mostrarTitulos(pelis);
 		
 		
 	}
